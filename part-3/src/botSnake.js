d0552e9c6748a2d4ae70c6377a97e77541588ee6
SyncedTogether <165613775+TheWilsonDev@users.noreply.github.com>
2025-05-15 17:04:54 -0400

    Revert "some modifications"
    This reverts commit 52b32a6366883609835dc209064fb85c2be2f72e.

--- part-3/src/botSnake.js
+++ part-3/src/botSnake.js
@@ -5,10 +5,10 @@
  * @param  {Number} x         coordinate
  * @param  {Number} y         coordinate
  */
-BotSnake = function (game, spriteKey, x, y) {
-  Snake.call(this, game, spriteKey, x, y);
-  this.trend = 1;
-};
+BotSnake = function(game, spriteKey, x, y) {
+    Snake.call(this, game, spriteKey, x, y);
+    this.trend = 1;
+}
 
 BotSnake.prototype = Object.create(Snake.prototype);
 BotSnake.prototype.constructor = BotSnake;
@@ -18,14 +18,14 @@ BotSnake.prototype.constructor = BotSnake;
  * can turn randomly
  */
 BotSnake.prototype.tempUpdate = BotSnake.prototype.update;
-BotSnake.prototype.update = function () {
-  this.head.body.setZeroRotation();
+BotSnake.prototype.update = function() {
+    this.head.body.setZeroRotation();
 
-  //ensure that the bot keeps rotating in one direction for a
-  //substantial amount of time before switching directions
-  if (Util.randomInt(1, 20) == 1) {
-    this.trend *= -1;
-  }
-  this.head.body.rotateRight(this.trend * this.rotationSpeed);
-  this.tempUpdate();
-};
+    //ensure that the bot keeps rotating in one direction for a
+    //substantial amount of time before switching directions
+    if (Util.randomInt(1,20) == 1) {
+        this.trend *= -1;
+    }
+    this.head.body.rotateRight(this.trend * this.rotationSpeed);
+    this.tempUpdate();
+}
